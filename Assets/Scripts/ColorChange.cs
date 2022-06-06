260f6c10adf9f91020a2e0087ee547a42cfd7e84
mengelhardt21 <36308565+mengelhardt21@users.noreply.github.com>
2022-06-06 01:59:12 -0400

    Initial Project Upload

---
+++ Assets/Scripts/ColorChange.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour {
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Sphere")
+        {
+            transform.GetComponent<Renderer>().material.color = Color.green;
+        }
+    }
+}
